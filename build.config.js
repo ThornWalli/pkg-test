dc72a502a95c0dbb5ce765d07a1f90912eb964a9
Thorn Walli <lammpee@gmail.com>
2025-02-07 13:46:04 +0100

    chore(build): update build config

--- build.config.js
+++ build.config.js
@@ -1,4 +1,18 @@
-export default {
-  failOnWarn: false,
-  externals: []
-};
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+  entries: [
+    { builder: 'mkdist', input: './src', pattern: ['**/*.js'], format: 'cjs', loaders: ['js'], ext: 'cjs' },
+    { builder: 'mkdist', input: './src', pattern: ['**/*.js'], format: 'esm', loaders: ['js'], ext: 'js' }
+  ],
+  declaration: true,
+  clean: true,
+  externals: [],
+  rollup: {
+    esbuild: {
+      target: 'esnext'
+    },
+    emitCJS: false,
+    cjsBridge: true
+  }
+});
